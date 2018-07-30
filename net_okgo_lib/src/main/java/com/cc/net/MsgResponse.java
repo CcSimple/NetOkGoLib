f855678bdb58713fe1a70b0fc3225b457fcef5c5
CcSimple <840054486@qq.com>
2018-07-30 16:55:29 +0800

    查看提交失败原因

--- net_okgo_lib/src/main/java/com/cc/net/MsgResponse.java
+++ net_okgo_lib/src/main/java/com/cc/net/MsgResponse.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 /**
  * @author: Administrator on 2018-04-16 15:29
  * @github: https://github.com/CcSimple
- * @describe:  常用提示返回格式(code,msg) aaaa
+ * @describe:  常用提示返回格式(code,msg)
+ * 提交失败的原因????
  */
 
 public class MsgResponse<T> implements Serializable {
