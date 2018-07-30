5671da213a54041b458d31186c35c6b7d6bb59df
CcSimple <840054486@qq.com>
2018-07-30 16:49:57 +0800

    添加注释说明

--- net_okgo_lib/src/main/java/com/cc/net/MsgResponse.java
+++ net_okgo_lib/src/main/java/com/cc/net/MsgResponse.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * @author: Administrator on 2018-04-16 15:29
  * @github: https://github.com/CcSimple
- * @describe:  常用提示返回格式(code,msg)
+ * @describe:  常用提示返回格式(code,msg) aaaa
  */
 
 public class MsgResponse<T> implements Serializable {
